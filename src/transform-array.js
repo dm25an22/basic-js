9ee906caa0c2e49eef0b71ad0f4d93549926d75b
dm25an22 <dm25an22@gmail.com>
2021-03-02 12:18:20 +0100

    feat: add some functions

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,60 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error();
+  const arrClone = [...arr];
+
+  for (let i = 0; i < arrClone.length; i++) {
+
+    switch (arrClone[i]) {
+      case controlSequence['discard-next']:
+
+        if (arrClone[i + 1] !== undefined) {
+          arrClone.splice(i + 1, 1);
+        }
+        break;
+
+      case controlSequence['discard-prev']:
+        if (arrClone[i - 1] !== undefined) {
+          arrClone.splice(i - 1, 1);
+        }
+        break;
+
+      case controlSequence["double-next"]:
+        if (arrClone[i + 1] !== undefined) {
+          arrClone.splice(i, 0, arrClone[i + 1]);
+          i++;
+        }
+        break;
+
+      case controlSequence["double-prev"]:
+        if (arrClone[i - 1] !== undefined) {
+          arrClone.splice(i, 0, arrClone[i - 1]);
+          i++;
+        }
+        break;
+
+      default:
+        continue;
+    }
+  }
+
+  return arrClone.filter(it => {
+    if (
+        it === controlSequence["double-next"] ||
+        it === controlSequence["double-prev"] || 
+        it === controlSequence["discard-next"] || 
+        it === controlSequence["discard-prev"]
+        ) {
+      return false;
+    }
+    return true;
+  });
 };
+
+const controlSequence = {
+  ['discard-next']: '--discard-next',
+  ['discard-prev']: '--discard-prev',
+  ['double-next']: '--double-next',
+  ['double-prev']: '--double-prev',
+}
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,34 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  if (!date.toLocaleString()) throw new Error();
+  const month = date.getMonth() + 1;
+
+  switch (month) {
+    case 12:
+    case 1:
+    case 2:
+      return season.winter;
+
+    case 3:
+    case 4:
+    case 5:
+      return season.spring
+
+    case 6:
+    case 7:
+    case 8:
+      return season.summer
+    
+    default:
+      return season.autumn;
+  }
 };
+
+const season = {
+  winter: "winter",
+  spring: "spring",
+  summer: "summer",
+  autumn: "autumn",
+}
