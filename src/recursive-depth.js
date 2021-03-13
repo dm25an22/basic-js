11a8261f24239a46c1303a896053b4e50ab5e472
dm25an22 <dm25an22@gmail.com>
2021-03-13 19:51:17 +0100

    feat: recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(arr) {
-    
+  calculateDepth = (arr) => {
+    if (Array.isArray(arr)) {
+      return 1 + Math.max(...arr.map(it => {
+        if (it.length === 0) it.push("fill empty arr");
+        return this.calculateDepth(it);
+      }));
+    }
+    return 0;
   }
- 
 };
